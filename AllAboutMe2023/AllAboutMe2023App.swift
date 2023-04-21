0b497a469bfcfcb106159ebe1a50754e316309fd
Julia Bub <21.jtbub@gmail.com>
2023-04-21 00:48:47 -0400

    Initial Commit

---
+++ AllAboutMe2023/AllAboutMe2023App.swift
@@ -0,0 +1,17 @@
+//
+//  AllAboutMe2023App.swift
+//  AllAboutMe2023
+//
+//  Created by Julia Bub on 4/21/23.
+//
+
+import SwiftUI
+
+@main
+struct AllAboutMe2023App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AllAboutMe2023/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AllAboutMe2023
+//
+//  Created by Julia Bub on 4/21/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
